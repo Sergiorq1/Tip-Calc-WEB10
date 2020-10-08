1038b086c07c018f1a82cdcec97e24de6b571229
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-10-07 21:39:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-let button = document.getElementById("btn");
-button.addEventListener("click", function(){
-    let billAmount = document.getElementById("bill_amount").value;
-    let tipPercentage = document.getElementById("tip_percentage").value;
+let btn = document.getElementById('btn');
 
-    let tipAmount = document.getElementById("tip_amount").value = 0.01 * percentTip * orderTotal;
-    document.getElementById("total_bill").value = parseFloat(orderTotal) + parseFloat(amountTip);
+btn.addEventListener('click', function(){
+    let billAmount = parseFloat(document.getElementById('bill_amount').value);
+    let tipPercentage = parseFloat(document.getElementById('tip_percentage').value);
+    let people = parseFloat(document.getElementById('people').value);
+
+    let computedTip = billAmount / tipPercentage;
+    let totalBill = document.getElementById('total_bill').value = billAmount / people + computedTip / people;
+    let tipAmount = document.getElementById('tip_amount').value = computedTip;
 })
